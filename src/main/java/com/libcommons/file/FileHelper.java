94c9170d779c677086a42f0fecd1d610ecb10424
João Victor Tavares <joaojv497@hotmail.com>
2025-07-23 22:09:06 -0300

    Up

--- src/main/java/com/libcommons/file/FileHelper.java
+++ src/main/java/com/libcommons/file/FileHelper.java
@@ -4,70 +4,69 @@ import com.libcommons.classes.ServiceException;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.file.*;
 
 @Component
 @NoArgsConstructor
 public class FileHelper {
 
-    private final String DEFAULT_TEMP_DIR = "temp";
+    private String tempDirName = "temp";
+
+    public FileHelper(String tempDirName) {
+        this.tempDirName = tempDirName;
+    }
 
     public Path getOrCreateTempDirectory() {
-        Path tempDir = get(DEFAULT_TEMP_DIR);
-
-        if (notExists(tempDir)) {
-            createDirectory(tempDir);
+        Path tempDir = Paths.get(tempDirName);
+        if (Files.notExists(tempDir)) {
+            try {
+                Files.createDirectories(tempDir);
+            } catch (IOException e) {
+                throw new ServiceException("Erro ao criar diretório temporário: " + tempDir, "FileHelper", "getOrCreateTempDirectory", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
         }
-
         return tempDir;
     }
 
-    public Path get(String first) {
-        return Paths.get(first);
-    }
-
-    public void writeContentToFile(File file, String content) throws IOException {
-        Files.write(file.toPath(), content.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
-    }
-
-    public File createTempFileWithContent(String prefix, String extension, String content) throws IOException {
-        File file = createTempFile(prefix, extension);
-        writeContentToFile(file, content);
-        return file;
-    }
-
-    public boolean deleteFile(File file) {
-        return file.delete();
-    }
-
-    private Path createDirectory(Path dir) {
-        try {
-            return Files.createDirectory(dir);
-        } catch (IOException e) {
-            throw new ServiceException("Erro ao criar diretório", "?", "?", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    private boolean notExists(Path path) {
-        return Files.notExists(path);
-    }
-
-    public File createTempFile(String prefix, String extension) throws IOException {
+    public Path createTempFile(String prefix, String extension) throws IOException {
         Path tempDir = getOrCreateTempDirectory();
-        String sanitized = sanitizeFileName(prefix);
-        String filename = sanitized + "." + extension;
-        Path filePath = tempDir.resolve(filename);
-        Files.createFile(filePath);
-        return filePath.toFile();
+        return Files.createTempFile(tempDir, sanitizeFileName(prefix) + "_", "." + extension);
+    }
+
+    public Path saveStringToFile(String prefix, String extension, String content) throws IOException {
+        Path tempFile = createTempFile(prefix, extension);
+        Files.writeString(tempFile, content, StandardOpenOption.TRUNCATE_EXISTING);
+        return tempFile;
+    }
+
+    public Path saveMultipartFileToTemp(MultipartFile file, String prefix) throws IOException {
+        String extension = getFileExtension(file.getOriginalFilename());
+        Path tempFile = createTempFile(prefix, extension);
+        Files.copy(file.getInputStream(), tempFile, StandardCopyOption.REPLACE_EXISTING);
+        return tempFile;
+    }
+
+    public void writeContentToFile(Path filePath, String content) throws IOException {
+        Files.writeString(filePath, content, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+    public boolean deleteFile(Path filePath) {
+        try {
+            return Files.deleteIfExists(filePath);
+        } catch (IOException e) {
+            return false;
+        }
     }
 
     private String sanitizeFileName(String fileName) {
         return fileName.replaceAll("[^a-zA-Z0-9-_\\.]", "_");
+    }
+
+    private String getFileExtension(String filename) {
+        int dotIndex = filename.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : filename.substring(dotIndex + 1);
     }
 }
